972af02cedd1c0ff7b514640cb94896040662c4d
Leopold Haller <leo@agentic.ai>
2023-09-17 08:48:59 -0700

    Add type inference to invokables.

--- src/enact/registration.py
+++ src/enact/registration.py
@@ -14,20 +14,68 @@
 
 """A unified registration function."""
 
-from typing import Callable, Type, TypeVar, Union, cast
+import inspect
+from typing import Callable, Optional, Tuple, Type, TypeVar, Union, cast
 
-from enact import function_wrappers, interfaces, resource_registry
+from enact import function_wrappers
+from enact import interfaces
+from enact import invocations
+from enact import resource_registry
 
 
-Registerable = Union[Type[interfaces.ResourceBase], Callable]
-
+Registerable = Union[
+  Type[interfaces.ResourceBase],
+  Callable]
 
 RegisterableT = TypeVar('RegisterableT', bound=Registerable)
+
+
+def _to_python_type(t) -> Optional[Type]:
+  """Try to extract a python type from a type annotation."""
+  if t is None:
+    return type(None)
+  if t is inspect.Signature.empty:
+    return None
+  if isinstance(t, type):
+    return t
+  if hasattr(t, '__origin__'):  # Handle generics
+    return t.__origin__
+  return None
+
+
+def _infer_types(invokable: Type[invocations._InvokableBase]) -> Tuple[
+    Optional[Type], Optional[Type]]:
+  """Infer types from the invokable."""
+  signature = inspect.signature(invokable.call)
+  output_type = _to_python_type(signature.return_annotation)
+  params = signature.parameters
+  if 'self' not in params:
+    raise TypeError(
+      'Invokable "call" function must have a "self" parameter.')
+  if len(params) > 2:  # We count self here.
+    raise TypeError(
+      'Invokable "call" function must have at most one non-self parameter.')
+  if len(params) == 1:
+    input_type: Optional[Type] = type(None)
+  else:
+    (_, param) = params.values()
+    if param.kind not in (param.POSITIONAL_ONLY,
+                          param.POSITIONAL_OR_KEYWORD):
+      raise TypeError(
+        'Invokable "call" function must have a single '
+        'non-self positional parameter.')
+    input_type = _to_python_type(param.annotation)
+  return input_type, output_type
 
 
 def register(registerable: RegisterableT) -> RegisterableT:
   """Register a resource class, wrapper or python callable."""
   if (isinstance(registerable, type) and
+      issubclass(registerable, invocations._InvokableBase)):  # pylint: disable=protected-access
+    input_type, output_type = _infer_types(registerable)
+    return cast(RegisterableT, invocations.typed_invokable(
+      input_type, output_type)(registerable))
+  elif (isinstance(registerable, type) and
       issubclass(registerable, interfaces.ResourceBase)):
     return cast(RegisterableT, resource_registry.register(registerable))
   elif callable(registerable):
---
+++ tests/test_registration.py
@@ -0,0 +1,97 @@
+# Copyright 2023 Agentic.AI Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests registration module."""
+
+import unittest
+
+import enact
+
+class RegistrationTest(unittest.TestCase):
+  """Tests for the enact.register function."""
+
+  def test_type_inference(self):
+    """Tests invokable type inference."""
+    @enact.register
+    class MyInvokable(enact.Invokable):
+      """Invokable for testing."""
+
+      def call(self, x: int) -> str:
+        return str(x)
+
+    self.assertEqual(MyInvokable.get_input_type(), int)
+    self.assertEqual(MyInvokable.get_output_type(), str)
+
+  def test_type_inference_none_return(self):
+    """Tests invokable type inference if none is returned."""
+    @enact.register
+    class MyInvokable(enact.Invokable):
+      """Invokable for testing."""
+
+      def call(self, x: int) -> None:
+        pass
+
+    self.assertEqual(MyInvokable.get_input_type(), int)
+    self.assertEqual(MyInvokable.get_output_type(), type(None))
+
+  def test_type_inference_no_type(self):
+    """Tests invokable type inference if no types are set."""
+    @enact.register
+    class MyInvokable(enact.Invokable):
+      """Invokable for testing."""
+
+      def call(self, x):
+        pass
+
+    self.assertEqual(MyInvokable.get_input_type(), None)
+    self.assertEqual(MyInvokable.get_output_type(), None)
+
+  def test_type_inference_no_arg(self):
+    """Tests invokable type inference if the call function has no args."""
+    @enact.register
+    class MyInvokable(enact.Invokable):
+      """Invokable for testing."""
+      def call(self):
+        pass
+
+    self.assertEqual(MyInvokable.get_input_type(), type(None))
+    self.assertEqual(MyInvokable.get_output_type(), None)
+
+  def test_register_too_many_args(self):
+    """Test too many argument errors on invokables."""
+    class TooManyArgs(enact.Invokable):
+      """Invokable for testing."""
+      def call(self, x, y):
+        pass
+    with self.assertRaises(TypeError):
+      enact.register(TooManyArgs)
+
+  def test_register_keyword_only_args(self):
+    """Test wrong kind error on invokables."""
+    class KeywordOnly(enact.Invokable):
+      """Invokable for testing."""
+      def call(self, *, x):
+        pass
+    with self.assertRaises(TypeError):
+      enact.register(KeywordOnly)
+
+  def test_register_no_self_arg(self):
+    """Test wrong kind error on invokables."""
+    class StaticCall(enact.Invokable):
+      """Invokable for testing."""
+      @staticmethod
+      def call(x):
+        pass
+    with self.assertRaises(TypeError):
+      enact.register(StaticCall)
