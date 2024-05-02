bd72ad1f808ac24d814356e36c3852a8055427df
Leopold Haller <leo@agentic.ai>
2024-05-02 11:02:00 -0700

    Read version directly from importlib.

--- src/enact/_version.py
+++ src/enact/_version.py
@@ -1,2 +1,16 @@
 """Version information for Enact."""
-__version__ = '0.4.6'
+
+from importlib import metadata
+import logging
+
+
+PKG_NAME = 'enact'
+
+
+try:
+  __version__ = metadata.version(PKG_NAME)
+except metadata.PackageNotFoundError:
+  __version__ = '0.0.0'
+  logging.warning(
+    'Could not find installed version for %s, using %s',
+    PKG_NAME, __version__)
