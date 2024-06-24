191e2ffa4d96d2572ba0b8aadddd0993b6ea84e5
Leopold Haller <leo@agentic.ai>
2024-06-24 11:18:44 -0700

    Fix copyright notice.

--- src/enact/types.py
+++ src/enact/types.py
@@ -1,4 +1,4 @@
-# Copyright 2023 Agentic.AI Corporation.
+# Copyright 2024 Agentic.AI Corporation.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
